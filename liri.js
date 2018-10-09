50bf9e40d3f48f1d568b0b5064d4c687875bf0d4
Marcus Aurelius <markmayfield16@gmail.com>
2018-10-09 09:46:47 -0500

    initial commit

---
+++ liri.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+
+//Spotify
+var request = require("request");
+var moment = require("moment");
+var chalk = require("chalk");
+
+
+
+var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+var keys
+
